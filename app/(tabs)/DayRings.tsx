4cb7db6a38faec40ce8b69f02f893ec7dc260aee
DougDelaney <xplorr@pm.me>
2025-05-08 00:16:21 -0700

    feat: import ActivityKey type from calendarStore for DayRings component

--- app/(tabs)/DayRings.tsx
+++ app/(tabs)/DayRings.tsx
@@ -1,6 +1,7 @@
 //  DayRings.tsx
 import Svg, { Circle, Text } from 'react-native-svg';
 import { ringPalette } from '@/constants/ringPalette';
+import { ActivityKey } from './calendarStore';
 const size = 34;
 const stroke = 3;
 const gap = 1.5;

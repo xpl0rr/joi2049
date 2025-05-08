51df08ca833b58fbd08b8a05e86c07dfc1309ab4
DougDelaney <xplorr@pm.me>
2025-05-08 09:17:42 -0700

    refactor: remove medium weight from collapsible chevron icon

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -20,7 +20,6 @@ export function Collapsible({ children, title }: PropsWithChildren & { title: st
         <IconSymbol
           name="chevron.right"
           size={18}
-          weight="medium"
           color={theme === 'light' ? Colors.light.icon : Colors.dark.icon}
           style={{ transform: [{ rotate: isOpen ? '90deg' : '0deg' }] }}
         />
